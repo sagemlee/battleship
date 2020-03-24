d53065a7747fb7283f18af5533d2f47c20af1a6e
Sage Lee <sagemcklee@gmail.com>
2020-03-24 14:45:54 -0600

    Add cell class and test coordinate attribute

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,10 +2,22 @@
 
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/card'
+
 require './lib/ship'
 require './lib/cell'
 
 class CellTest < Minitest::Test
   def setup
-    @cell = cell.new("B4")
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_it_has_coordinate
+    assert_equal "B4", @cell.coordinate
+  end 
+
+
+end
