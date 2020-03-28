c8ae40e6b80552bc1440cde70186ccd10e656cf6
Kiera Allen <kieraaallen@gmail.com>
2020-03-27 20:56:32 -0600

    Test that ship can be placed on multiple cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -100,8 +100,20 @@ class BoardTest < Minitest::Test
     assert_equal cruiser, cell_1.ship
   end
 
-  # def test_that_a_ship_is_on_multiple_cells
-  #
-  # assert_equal true, cell_3.ship == cell_2.ship
-  # end
+  def test_that_a_ship_is_on_multiple_cells
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+
+    cell_1.ship
+    cell_2.ship
+    cell_3.ship
+  assert_equal true, cell_3.ship == cell_2.ship
+  end
 end
